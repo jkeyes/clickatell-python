2bb5bb323317d326c852354134bbb6cbf2ccb54b
Martin Thorsen Ranang <mtr@ranang.org>
2011-02-14 15:21:48 +0100

    Added a setuptools script, to enable installation through pip or easy_install.

---
+++ setup.py
@@ -0,0 +1,27 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    """Utility function to read the README file.
+    """
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "clickatell",
+    version = "0.5.0",
+    author = "John Keyes",
+    author_email = "johnkeyes@gmail.com",
+    description = ("The clickatell module."),
+    license = "Apache",
+    keywords = "clickatell sms gateway",
+    url = "http://keyes.ie/clickatell",
+    packages=['clickatell'],
+    package_dir={'clickatell': '.'},
+    long_description=read('README'),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
